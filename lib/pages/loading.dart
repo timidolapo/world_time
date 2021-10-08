4cc04a23d3b3571ee4e49177a4c5fb7a44b8ec08
Timileyin <ikumapayioluwatimileyin@gmail.com>
2021-10-09 00:43:29 +0100

    completed the home page

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -20,6 +21,7 @@ class _LoadingState extends State<Loading> {
         'location': instance.location,
         'flag': instance.flag,
         'time': instance.time,
+        'isDaytime': instance.isDaytime,
       },
     );
   }
@@ -33,9 +35,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('Loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
       ),
     );
   }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -7,6 +7,7 @@ class WorldTime {
   late String time; // time in that location
   String flag; //url to an assest flag icon
   String url; //location url for api endpoint
+  late bool isDaytime; //true or False if its daytim or not
 
   WorldTime({required this.location, required this.flag, required this.url});
 
@@ -29,6 +30,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
 //set the time property
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     } catch (e) {
       print('Caught an error: $e');
