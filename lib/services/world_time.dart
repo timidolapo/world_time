7ec09eca3eee286cd7a794223703897fbf96149a
Timileyin <ikumapayioluwatimileyin@gmail.com>
2021-10-08 23:38:21 +0100

    updated the home page

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location; // location name for the UI
@@ -28,7 +29,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
 //set the time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
       print('Caught an error: $e');
       time = 'Could not get the time data';
